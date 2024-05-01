79dd2ded1e3dfd3183cb9626e78d37b460330c8c
larrry <mail@larrry.ink>
2024-05-01 14:57:20 -0400

    Remove Lua game code

--- Source/main.lua
+++ Source/main.lua
@@ -6,13 +6,6 @@ local ui <const> = playdate.ui
 local isCrankDocked <const> = playdate.isCrankDocked
 local drawFPS <const> = playdate.drawFPS
 
-
---for i=1,100 do
---  local ant = Ant()
---  ant.sprite:moveTo(math.random(60,340), math.random(20,220))
---  ant:add()
---end
-
 local engineUpdate = cupdate
 
 function playdate.update()
